65531ef5fd4beec63965bb990febc5e937574cb2
STukac <150451795+STukac@users.noreply.github.com>
2024-06-01 15:07:46 +0200

    Update config.tcl

--- src/config.tcl
+++ src/config.tcl
@@ -16,11 +16,11 @@ set ::env(PL_TARGET_DENSITY) 0.6
 
 # CLOCK_PERIOD - Increase this in case you are getting setup time violations.
 # The value is in nanoseconds, so 20ns == 50MHz.
-set ::env(CLOCK_PERIOD) "200"
+set ::env(CLOCK_PERIOD) "2000"
 
 # Hold slack margin - Increase them in case you are getting hold violations.
-set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 2
-set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) 1
+set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 200
+set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) 100
 
 # RUN_LINTER, LINTER_INCLUDE_PDK_MODELS - Disabling the linter is not recommended!
 set ::env(RUN_LINTER) 1
